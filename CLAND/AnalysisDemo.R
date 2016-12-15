bc7bdfdc39e712d41e7d71b4457c94520e62c26e
zoox101 <william@thebookers.net>
2016-12-15 10:57:46 -0600

    CLAND Testing

--- CLAND/AnalysisDemo.R
+++ CLAND/AnalysisDemo.R
@@ -1,9 +1,14 @@
 x = runif(100000); y = c()
 for(i in 1:100000) y[i] = rbinom(1, size = 100, prob = x[i])
 df = data.frame(x, y)
-sub = subset(df, df$y == 7)
-xgiven7 = sub$x
-sorted = sort(xgiven7)
+sub = subset(df, df$y == 1)
+xgiveny = sub$x
+sorted = sort(xgiveny)
 low = trunc(length(sorted) * 0.025)
 high = trunc(length(sorted) * 0.975) + 1
 sorted[low]; sorted[high]
+
+
+
+highlow(function(x) {mybinhigh(x, successes=1, total=100)})
+highlow(function(x) {mybinlow(x, successes=1, total=100)})
