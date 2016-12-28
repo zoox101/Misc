8f07c242a4ffff9d09a59786ad8f6af778fa4200
Will Booker <william@williams-mbp.attlocal.net>
2016-12-28 15:05:48 -0600

    Updated User Class

--- WBToolBox/src/User.java
+++ WBToolBox/src/User.java
@@ -1,3 +1,4 @@
+package ResponseModel;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,8 +16,9 @@ public class User {
 	 * @param type -- The prompt for the type of input being read.
 	 * @return a string given by the user.
 	 */
-	private static String get(String type) {
+	private static String get(String prompt) {
 		while(true) { 
+			if (prompt != null) System.out.print(prompt);
 			try {return reader.readLine();} 
 			catch(IOException e) {}
 		}
@@ -26,50 +28,53 @@ public class User {
 	 * Gets a string from the user.
 	 * @return a string given by the user.
 	 */
-	public static String getString() {
-		return get("a string");
-	}
+	public static String getString(String prompt) {return get("a string");}
+	public static String getString() {return getString(null);}
 
 	/**
 	 * Gets an integer from the user.
 	 * @return an integer from the user.
 	 */
-	public static int getInteger() {
+	public static int getInteger(String prompt) {
 		while(true) {
-			try{return Integer.parseInt(get("an integer"));}
+			try{return Integer.parseInt(get(prompt));}
 			catch(NumberFormatException e) {System.out.println("Invalid input");};
 		}
 	}
+	public static int getInteger() {return getInteger(null);}
 	
 	/**
 	 * Gets a double from the user.
 	 * @return a double from the user.
 	 */
-	public static double getDouble() {
+	public static double getDouble(String prompt) {
 		while(true) {
-			try{return Double.parseDouble(get("a double"));}
+			try{return Double.parseDouble(get(prompt));}
 			catch(NumberFormatException e) {System.out.println("Invalid input");};
 		}
 	}
+	public static double getDouble() {return getDouble(null);}
 	
 	/**
 	 * Gets a long from the user.
 	 * @return a double from the user.
 	 */
-	public static long getLong() {
+	public static long getLong(String prompt) {
 		while(true) {
-			try{return Long.parseLong(get("a long"));}
+			try{return Long.parseLong(get(prompt));}
 			catch(NumberFormatException e) {System.out.println("Invalid input");};
 		}
 	}
+	public static long getLong() {return getLong(null);}
 	
 	/**
 	 * Gets a boolean from the user.
 	 * @return a boolean from the user.
 	 */
-	public static boolean getBoolean() {
-		String input = get("yes/no").toLowerCase();
+	public static boolean getBoolean(String prompt) {
+		String input = get(prompt).toLowerCase();
 		if(input.charAt(0) == 'y') return true;
 		else return false;
 	}
+	public static boolean getBoolean() {return getBoolean(null);}
 }
